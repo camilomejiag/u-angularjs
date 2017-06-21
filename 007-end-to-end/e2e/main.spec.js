f0d297fef7c8ebb1333bff6e63193df29fcfc124
Camilo Mejia <camicasi@hotmail.com>
2017-06-21 11:07:38 -0500

    Solucion problema 7

--- 007-end-to-end/e2e/main.spec.js
+++ 007-end-to-end/e2e/main.spec.js
@@ -4,21 +4,46 @@ describe('The main view', function () {
   var page;
 
   function fillForm() {
-    page.title.sendKeys('Talos Business Card');
+  //Fill the form with valid information
+    element(by.id("input_0")).sendKeys("#caa2a2");
+    element(by.id("input_1")).sendKeys("#17346e");
+    element(by.id("title")).sendKeys("Hello From Talos");
+    element(by.id("description")).sendKeys("This is business card by Talos Digital S.A.S");
+    element(by.id("favoriteBtn")).click();
+    element(by.id("favoriteBtn")).click();
+    element(by.id("favoriteBtn")).click();
   }
 
-  beforeEach(function () {
-    browser.get('/index.html');
+  beforeAll(function () {
+    browser.get('http://localhost:3000/#/');
     page = require('./main.po');
+    fillForm();
   });
 
-  it('should be right data into directive', function() {
-    fillForm();
+  it('card directives should exist', function () {
     expect(page.directiveTitle.evaluate('title').isPresent()).toBe(true);
+    expect(page.directiveTitle.evaluate('content').isPresent()).toBe(true);
+    expect(page.directiveTitle.evaluate('actions').isPresent()).toBe(true);
+  });
+
+  it('should check the data of the controller is the same on the directive', function() {
+    //Validate the Title is the same on the directive and in the home
+    expect(element(by.model("homeCtrl.card.title")).getText()).toBe(element(by.id("title")).getText());
+
+    //Validate the description is the same on the directive and in the home
+    expect(element(by.model("homeCtrl.card.description")).getText()).toBe(element(by.id("description")).getText());
+
+    //Validate the background color is the same on the directive and in the home
+    expect(element(by.id('card')).getCssValue('background-color')).toEqual('rgba(202, 162, 162, 1)');
+
+    //Validate the text color is the same on the directive and in the home
+    expect(element(by.id('card')).getCssValue('color')).toEqual('rgba(23, 52, 110, 1)');
+
   });
 
   it('should favorite list equal to 1', function () {
-    //other test here
+    expect(element.all(by.repeater('favorite in homeCtrl.favoriteList track by $index')).count()).not.toEqual(0);
+
   });
 
 });
--- 007-end-to-end/protractor.conf.js
+++ 007-end-to-end/protractor.conf.js
@@ -5,7 +5,6 @@ var paths = require('./.yo-rc.json')['generator-gulp-angular'].props.paths;
 // An example configuration file.
 exports.config = {
   // The address of a running selenium server.
-  seleniumAddress: 'http://localhost:4444/wd/hub',
   //seleniumServerJar: deprecated, this should be set on node_modules/protractor/config.json
 
   // Capabilities to be passed to the webdriver instance.
@@ -22,6 +21,6 @@ exports.config = {
   // Options to be passed to Jasmine-node.
   jasmineNodeOpts: {
     showColors: true,
-    defaultTimeoutInterval: 30000
+    defaultTimeoutInterval: 50000
   }
 };
